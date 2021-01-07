06588758389226896658bab705987a898424859d
jdda95 <j.delgado@orekaconsultores.com>
2021-01-07 18:11:36 -0500

    Respuestas de servidor a frontend

--- api/acuarelauser/controllers/acuarelauser.js
+++ api/acuarelauser/controllers/acuarelauser.js
@@ -1,8 +1,8 @@
-'use strict';
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
-const cors =  require('cors');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const verification = require("../../../middlewares/authJwt");
 
 /**
@@ -12,84 +12,102 @@ const verification = require("../../../middlewares/authJwt");
 
 module.exports = {
   async login(ctx) {
-    const {mail, pass} = ctx.request.body;
+    const { mail, pass } = ctx.request.body;
     let entity = await strapi.services.acuarelauser.findOne({ mail });
-    let result = await bcrypt.compare(pass, entity.password);
-    if (result) {
-        const token = jwt.sign({id: entity._id}, process.env.SECRET, {
-            expiresIn: 259200 // tres dias
+    if (entity) {
+      let result = await bcrypt.compare(pass, entity.password);
+      if (result) {
+        const token = jwt.sign({ id: entity._id }, process.env.SECRET, {
+          expiresIn: 259200, // tres dias
         });
-        ctx.body = {mail:entity.mail, id:entity._id};
-        ctx.send({token});
+        const user = { mail: entity.mail, id: entity._id };
+        ctx.send({
+          token,
+          msg: "user logged",
+          user,
+          ok: true,
+        });
+      }
     } else {
-        ctx.status = 400;
-        ctx.body = {status:"Credenciales Erróneas."};
+      ctx.status = 400;
+      ctx.body = { status: "Credenciales Erróneas.", ok: false };
     }
   },
   async invitation(ctx) {
     const { mail, token } = ctx.request.body;
 
-    const { message, result, status} = await verification.verifyJwt(token);
+    const { message, result, status } = await verification.verifyJwt(token);
 
     if (!result) {
-        ctx.status = status;
-        ctx.body = {status: message};
+      ctx.status = status;
+      ctx.body = { status: message };
     } else {
-        let entity = await strapi.services.acuarelauser.findOne({ mail });
-        if (entity) {
-            ctx.status = 400;
-            ctx.body = {status:"User Already Exists"};
-        }
-        else {
-            entity = await strapi.services.acuarelauser.create({mail:mail});
-            ctx.body = {status: "Invitation Created"};
-        }
+      let entity = await strapi.services.acuarelauser.findOne({ mail });
+      if (entity) {
+        ctx.status = 400;
+        ctx.body = { status: "User Already Exists" };
+      } else {
+        entity = await strapi.services.acuarelauser.create({ mail: mail });
+        ctx.body = { status: "Invitation Created" };
+      }
     }
   },
   async register(ctx) {
     const { mail, pass } = ctx.request.body;
-
     let entity = await strapi.services.acuarelauser.findOne({ mail });
     if (entity) {
-        ctx.status = 400;
-        ctx.body = {status:"User Already Exists"};
+      ctx.status = 400;
+      ctx.body = { status: "User Already Exists", ok: false };
     }
     if (!entity) {
-        const hashedPassword = await bcrypt.hash(pass, 10);
+      const hashedPassword = await bcrypt.hash(pass, 10);
 
-        entity = await strapi.services.acuarelauser.create( {mail:mail, password:hashedPassword });
-        const token = jwt.sign({id: entity._id}, process.env.SECRET, {
-            expiresIn: 259200 // tres dias
-        });
-        ctx.send({token});
-        ctx.body = {status: "User Created", mail:entity.mail, id:entity._id};
+      entity = await strapi.services.acuarelauser.create({
+        mail: mail,
+        password: hashedPassword,
+      });
+      const token = jwt.sign({ id: entity._id }, process.env.SECRET, {
+        expiresIn: 259200, // tres dias
+      });
+      ctx.send({
+        token,
+        status: "User Created",
+        user: { mail: entity.mail, id: entity._id },
+        ok: true,
+      });
     }
   },
   async invitationregister(ctx) {
     const { mail, pass, token } = ctx.request.body;
 
-    const { message, result, status} = await verification.verifyJwt(token);
+    const { message, result, status } = await verification.verifyJwt(token);
 
     if (!result) {
-        ctx.status = status;
-        ctx.body = {status: message};
+      ctx.status = status;
+      ctx.body = { status: message };
     } else {
-        let entity = await strapi.services.acuarelauser.findOne({ mail });
-        if (!entity) {
-            ctx.status = 400;
-            ctx.body = {status:"Invalid Invitation"};
-        }
-        if (entity) {
-            const hashedPassword = await bcrypt.hash(pass, 10);
-            entity.password = hashedPassword;
-            let entity_id = entity._id;
-            entity = await strapi.services.acuarelauser.update( { _id:entity_id }, entity);
-            const token = jwt.sign({id: entity._id}, process.env.SECRET, {
-                expiresIn: 259200 // tres dias
-            });
-            ctx.send({token});
-            ctx.body = {status: "User Created", mail:entity.mail, id:entity_id};
-        }
+      let entity = await strapi.services.acuarelauser.findOne({ mail });
+      if (!entity) {
+        ctx.status = 400;
+        ctx.body = { status: "Invalid Invitation" };
+      }
+      if (entity) {
+        const hashedPassword = await bcrypt.hash(pass, 10);
+        entity.password = hashedPassword;
+        let entity_id = entity._id;
+        entity = await strapi.services.acuarelauser.update(
+          { _id: entity_id },
+          entity
+        );
+        const token = jwt.sign({ id: entity._id }, process.env.SECRET, {
+          expiresIn: 259200, // tres dias
+        });
+        ctx.send({
+          token,
+          status: "User Created",
+          user: { mail: entity.mail, id: entity_id },
+        });
+      }
     }
   },
 };
--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://198.211.114.51/api",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '06b77711f7a956f6477095a04d7034de'),
+      url: "http://198.211.114.51/dashboard",
+      secret: env("ADMIN_JWT_SECRET", "06b77711f7a956f6477095a04d7034de"),
     },
   },
 });

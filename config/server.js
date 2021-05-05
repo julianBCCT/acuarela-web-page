b63024f55aa07a6fd09db5ac82e7fa3268bbacd9
LordExodia <ks.estupinan@uniandes.edu.co>
2021-05-05 17:18:10 -0500

    Server cambios v2

--- config/server.js
+++ config/server.js
@@ -3,10 +3,9 @@ require('events').EventEmitter.defaultMaxListeners = 20;
 module.exports = ({ env }) => ({
   host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
-  url: process.env.API,
+  url: env('API'),
   admin: {
     auth: {
-      url: process.env.DASHBOARD,
       secret: env('ADMIN_JWT_SECRET', '06b77711f7a956f6477095a04d7034de'),
     },
   },

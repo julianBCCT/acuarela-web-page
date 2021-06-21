9fc2cacdc0bc9291ed8081d11e80d0b20e46baec
julian <dreinovcorp@gmail.com>
2021-06-21 14:23:06 -0500

    Sort post muro

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -110,7 +110,6 @@ module.exports = {
       let entity = await strapi
         .query('post')
         .model.find()
-        .sort({ date: -1 })
         .skip(parseInt(pageNo))
         .limit(parseInt(limit))
         .populate('acuarelauser', ['name', 'id', 'photo'])

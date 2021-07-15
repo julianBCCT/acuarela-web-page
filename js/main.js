95338c881df9e28ef2d10e65c730f179e4ec2318
cristianrincon-ui <cristianrincon.ui@gmail.com>
2021-07-14 20:17:56 -0500

    Style fixes after review

--- js/main.js
+++ js/main.js
@@ -4,51 +4,55 @@
 
 // General toggle
 function toggleTarget(target) {
-  	$("[data-toggle='" + target + "']").toggleClass("active");
+    $("[data-toggle='" + target + "']").toggleClass("active");
 }
 
 // Accordion
 function toggleAccordion(target) {
-	$("[data-toggle='" + target + "']")
-		.siblings()
-		.removeClass("active");
-	$("[data-toggle='" + target + "']")
-		.siblings()
-		.find(".content")
-		.slideUp();
+    $("[data-toggle='" + target + "']")
+        .siblings()
+        .removeClass("active");
+    $("[data-toggle='" + target + "']")
+        .siblings()
+        .find(".content")
+        .slideUp();
 
-	$("[data-toggle='" + target + "']").toggleClass("active");
-	$("[data-toggle='" + target + "']")
-		.find(".content")
-		.slideToggle();
+    $("[data-toggle='" + target + "']").toggleClass("active");
+    $("[data-toggle='" + target + "']")
+        .find(".content")
+        .slideToggle();
 }
 
 $("#contact__form").validate({
-	ignore: "",
-	rules: {
-		name: "required",
-		phone: {
-			digits: true
-		}
-	},
-	messages: {
-		name: "Mensaje de error",
-	},
-	highlight: function (element) {
-		$(element).addClass('error');
-		$(element).parent('div').addClass('error');
-	},
-	unhighlight: function (element) {
-		$(element).removeClass('error').addClass('success');
-		$(element).parent('div').removeClass('error').addClass('success');
-
-	},
-	submitHandler: function (form) {
-		$("#contact__form button").attr("disabled", true);
-		$("#contact__form button").text("Enviando");
-		// $("#contact__form").ajaxSubmit({
-		//   dataType: "json",
-		//   success: function (data) {},
-		// });
-	},
+    ignore: "",
+    rules: {
+        name: "required",
+        phone: {
+            digits: true,
+        },
+    },
+    messages: {
+        name: "Mensaje de error",
+    },
+    highlight: function (element) {
+        $(element).addClass("error");
+        $(element).parent("div").addClass("error");
+    },
+    unhighlight: function (element) {
+        $(element).removeClass("error").addClass("success");
+        $(element).parent("div").removeClass("error").addClass("success");
+    },
+    submitHandler: function (form) {
+        $("#contact__form button").attr("disabled", true);
+        $("#contact__form button").text("Enviando");
+        setTimeout(() => {
+            $("#contact__form").fadeOut();
+            $(".contact__texts").hide();
+            $(".contact__success").fadeIn();
+        }, 500);
+        // $("#contact__form").ajaxSubmit({
+        //   dataType: "json",
+        //   success: function (data) {},
+        // });
+    },
 });

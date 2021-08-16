e06a770f05c7bad9eee5bc8f75045ff5f1e11654
jdda95 <j.delgado@orekaconsultores.com>
2021-08-16 16:41:26 -0500

    Emailprovider Inscripciones agregar campos

--- helpers/email_provider.js
+++ helpers/email_provider.js
@@ -9,6 +9,7 @@ async function send_email(to, from, replyTo, msg, subject) {
   const message = {
     from_email: from,
     subject: subject,
+    
     text: `Enlace de invitacion ${msg}`,
     to: [
       {
@@ -17,15 +18,14 @@ async function send_email(to, from, replyTo, msg, subject) {
       }
     ]
   };
-  /*
+  
   const response = await mailchimp.messages.send({ message });
   console.log(response);
-  if (response[0].status === 'sent')*/
-  return { ok: true, status: 200, code: 0, msg: 'Email Send Succesfully.' };/*
+  if (response[0].status === 'sent')
+    return { ok: true, status: 200, code: 0, msg: 'Email Send Succesfully.' };
   else {
     return { ok: false, status: 500, code: 1, msg: `Something wrong with the email:\n ${response[0].reject_reason}` };
   }
-*/
 }
 
 module.exports = {

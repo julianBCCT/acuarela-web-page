82b642317befff61a0ff008d7ba1eb6705e753e6
jdda95 <j.delgado@orekaconsultores.com>
2021-09-11 10:45:57 -0500

    Formularios Validados y con envio de correo automatico

--- js/main.js
+++ js/main.js
@@ -4,55 +4,67 @@
 
 // General toggle
 function toggleTarget(target) {
-    $("[data-toggle='" + target + "']").toggleClass("active");
+  $("[data-toggle='" + target + "']").toggleClass("active");
 }
 
 // Accordion
 function toggleAccordion(target) {
-    $("[data-toggle='" + target + "']")
-        .siblings()
-        .removeClass("active");
-    $("[data-toggle='" + target + "']")
-        .siblings()
-        .find(".content")
-        .slideUp();
+  $("[data-toggle='" + target + "']")
+    .siblings()
+    .removeClass("active");
+  $("[data-toggle='" + target + "']")
+    .siblings()
+    .find(".content")
+    .slideUp();
 
-    $("[data-toggle='" + target + "']").toggleClass("active");
-    $("[data-toggle='" + target + "']")
-        .find(".content")
-        .slideToggle();
+  $("[data-toggle='" + target + "']").toggleClass("active");
+  $("[data-toggle='" + target + "']")
+    .find(".content")
+    .slideToggle();
 }
 
 $("#contact__form").validate({
-    ignore: "",
-    rules: {
-        name: "required",
-        phone: {
-            digits: true,
-        },
+  ignore: "",
+  rules: {
+    name: "required",
+    phone: {
+      digits: true,
     },
-    messages: {
-        name: "Mensaje de error",
-    },
-    highlight: function (element) {
-        $(element).addClass("error");
-        $(element).parent("div").addClass("error");
-    },
-    unhighlight: function (element) {
-        $(element).removeClass("error").addClass("success");
-        $(element).parent("div").removeClass("error").addClass("success");
-    },
-    submitHandler: function (form) {
-        $("#contact__form button").attr("disabled", true);
-        $("#contact__form button").text("Enviando");
-        setTimeout(() => {
-            $("#contact__form").fadeOut();
-            $(".contact__texts").hide();
-            $(".contact__success").fadeIn();
-        }, 500);
-        // $("#contact__form").ajaxSubmit({
-        //   dataType: "json",
-        //   success: function (data) {},
-        // });
-    },
+    email: { required: true, email: true },
+    daycare_name: { required: true },
+    country: { required: true },
+    city: { required: true },
+  },
+  messages: {
+    name: "Nombre obligatorio.",
+    phone: "Número telefónico obligatorio.",
+    email: { required: "Correo obligatorio.", email: "Correo no valido." },
+    daycare_name: { required: "Daycare obligatorio." },
+    country: { required: "País obligatorio." },
+    city: { required: "Ciudad obligatoria." },
+  },
+  highlight: function (element) {
+    $(element).addClass("error");
+    $(element).parent("div").addClass("error");
+  },
+  unhighlight: function (element) {
+    $(element).removeClass("error").addClass("success");
+    $(element).parent("div").removeClass("error").addClass("success");
+  },
+  submitHandler: function (form) {
+    $("#contact__form button").attr("disabled", true);
+    $("#contact__form button").text("Enviando");
+    $("#contact__form").ajaxSubmit({
+      dataType: "json",
+      success: function (data) {
+        console.log(data);
+        console.log("Encviado");
+      },
+    });
+    setTimeout(() => {
+      $("#contact__form").fadeOut();
+      $(".contact__texts").hide();
+      $(".contact__success").fadeIn();
+    }, 500);
+  },
 });

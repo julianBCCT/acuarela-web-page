e9968134d6d01714b613107821bab85e65942805
Valentina <est.valentina.alvac@unimilitar.edu.co>
2021-09-21 21:00:13 -0500

    estilos slider e iconos redes sociales, tambien botones de appstore y playstore

--- js/app.js
+++ js/app.js
@@ -1,31 +1,13 @@
 window.addEventListener('load', function(){
-	new Glider(document.querySelector('.card'), {
+	new Glider(document.querySelector('.glider'), {
 		 // Mobile-first defaults
         slidesToShow: 1,
 		slidesToScroll: 1,
-        itemWidth: 160,
         draggable: true,
 		arrows: {
 			prev: '.carousel-prev',
 			next: '.carousel-next'
 		},
-		responsive: [
-			{
-			  // screens greater than >= 775px
-			  breakpoint: 768,
-			  settings: {
-				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 1,
-				slidesToScroll: 1
-			  }
-			},{
-			  // screens greater than >= 1024px
-			  breakpoint: 800,
-			  settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
-			  }
-			},
-		]
+		
 	});
 });

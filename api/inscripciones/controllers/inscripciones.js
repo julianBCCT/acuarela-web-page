67c95efd30a5adbdb6bd48beb179495a74246ab6
jdda95 <j.delgado@orekaconsultores.com>
2021-10-01 01:07:43 -0500

    edit commented

--- api/inscripciones/controllers/inscripciones.js
+++ api/inscripciones/controllers/inscripciones.js
@@ -17,35 +17,32 @@ module.exports = {
       child.attitudes = [];
       const kid = await strapi.services.children.create(child);
       const hashedPassword = await bcrypt.hash('123456', 10);
-      console.log(kid);
       let parents = [];
-      let guardians = [];
-      for (const parent of kid.parents) {
+      for (const parent of child.parents) {
         parent.password = hashedPassword;
         parent.status = true;
         let entity = await strapi.services.acuarelauser.create(parent);
         parents.push(entity);
       }
-      console.log(parents);
-      let kidfound = await strapi.services.children.findOne({ _id: kid.id });
-      console.log(kidFound);
-      if(kidfound){
-        const kidEdited = await strapi.services.children.update({ _id: id }, {parents: [parents[0].id, parents[1].id]});
-        console.log(kidEdited);
-        return ctx.send({
-          created: true,
-          status: 200,
-          child: kidEdited,
-          parents
-        });
-      }else{
-        return ctx.send({
-          ok: false,
-          status: 404,
-          code: 5,
-          msg: 'Child not found.',
-        });
-      }
+      return ctx.send({
+        ok: true,
+        status: 200,
+        code: 1,
+        kid,
+        parents
+      });
+      //   let kidfound = await strapi.services.children.findOne({ _id: kid.id });
+      //   if(kidfound){
+      //     const kidEdited = await strapi.services.children.update({ _id: id }, {parents: [parents[0].id, parents[1].id]});
+          
+      //   }else{
+      //     return ctx.send({
+      //       ok: false,
+      //       status: 404,
+      //       code: 5,
+      //       msg: 'Child not found.',
+      //     });
+      //   }
 
     } else return ctx.send(validToken);
   },

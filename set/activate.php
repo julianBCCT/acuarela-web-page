997aa10d774e733d5d4a12d57e8f3389fde43f57
jdda95 <j.delgado@orekaconsultores.com>
2021-10-25 12:38:32 -0500

    Formulario activacion de demos

---
+++ set/activate.php
@@ -0,0 +1,67 @@
+<?php
+    include '../includes/config.php';
+    $array = array();
+    extract($_POST);
+    $curl = curl_init();
+    $today = date("Y-m-d");
+    $final = date("Y-m-d", strtotime("+1 month", $time));
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://acuarelacore.com/api/daycares',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{
+        "name": "'.$daycare_name.'",
+        "active": true,
+        "country": "USA",
+        "state": "'.$state.'",
+        "city": "'.$city.'",
+        "license": "'.$licence.'"
+    }',
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json'
+    ),
+    ));
+    $daycare = curl_exec($curl);
+    $finalDaycare = json_decode($daycare);
+    curl_close($curl);
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://acuarelacore.com/api/acuarelausers/register',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{
+        "pass": "'.$password.'",
+        "lastname": "'.$lastname.'",
+        "name": "'.$name.'",
+        "mail": "'.$email.'",
+        "phone": "'.$phone.'",
+        "daycare": ["'.$finalDaycare->id.'"],
+        "status": true,
+        "rols":["'.$rols.'"],
+        "country": "USA",
+        "state": "'.$state.'",
+        "city": "'.$city.'",
+        "end_demo": "'.$final.'"
+    }',
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json'
+    ),
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $array['message'] = 1;
+    $array['user'] = json_decode($response);
+    $array['daycare'] = json_decode($daycare);
+    $a->sendDemoActiveEmail($name,$email,$password);
+    echo json_encode($array);

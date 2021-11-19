6af8fd110e436a9aaefadbeb61bd6e349a3122c2
jdda95 <j.delgado@orekaconsultores.com>
2021-11-18 19:49:41 -0500

    a

--- api/inscripciones/controllers/inscripciones.js
+++ api/inscripciones/controllers/inscripciones.js
@@ -13,23 +13,22 @@ module.exports = {
     let validToken = await verification.renew(token);
     if (validToken.ok) {
       child.status = true;
-      child.daycare = validToken.user.organization;
       child.attitudes = [];
       const kid = await strapi.services.children.create(child);
       console.log(kid);
-      // const hashedPassword = await bcrypt.hash("123456", 10);
-      // let parents = [];
-      // for (const parent of child.parents) {
-      //   parent.password = hashedPassword;
-      //   parent.status = true;
-      //   parent.rols = ["5ff790045d6f2e272cfd7394"];
-      //   let entity = await strapi.services.acuarelauser.create(parent);
-      //   parents.push(entity);
-      // }
-      // const kidEdited = await strapi.services.children.update(
-      //   { _id: kid.id },
-      //   { acuarelausers: [parents[0].id, parents[1].id] }
-      // );
+      const hashedPassword = await bcrypt.hash("123456", 10);
+      let parents = [];
+      for (const parent of child.parents) {
+        parent.password = hashedPassword;
+        parent.status = true;
+        parent.rols = ["5ff790045d6f2e272cfd7394"];
+        let entity = await strapi.services.acuarelauser.create(parent);
+        parents.push(entity);
+      }
+      const kidEdited = await strapi.services.children.update(
+        { _id: kid.id },
+        { acuarelausers: [parents[0].id, parents[1].id] }
+      );
       return ctx.send({
         ok: true,
         status: 200,

011f2f5e2a56291ac0291d2491f5fb089d193ae2
jdda95 <j.delgado@orekaconsultores.com>
2021-11-18 19:43:39 -0500

    Kid log inscripciones

--- api/inscripciones/controllers/inscripciones.js
+++ api/inscripciones/controllers/inscripciones.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-const bcrypt = require('bcryptjs');
-const verification = require('../../../middlewares/authJwt');
+const bcrypt = require("bcryptjs");
+const verification = require("../../../middlewares/authJwt");
 
 module.exports = {
   async completeInsc(ctx) {
@@ -16,24 +16,27 @@ module.exports = {
       child.daycare = validToken.user.organization;
       child.attitudes = [];
       const kid = await strapi.services.children.create(child);
-      const hashedPassword = await bcrypt.hash('123456', 10);
+      console.log(kid);
+      const hashedPassword = await bcrypt.hash("123456", 10);
       let parents = [];
       for (const parent of child.parents) {
         parent.password = hashedPassword;
         parent.status = true;
-        parent.rols = ['5ff790045d6f2e272cfd7394'];
+        parent.rols = ["5ff790045d6f2e272cfd7394"];
         let entity = await strapi.services.acuarelauser.create(parent);
         parents.push(entity);
       }
-      const kidEdited = await strapi.services.children.update({ _id: kid.id }, {acuarelausers: [parents[0].id, parents[1].id]});
+      const kidEdited = await strapi.services.children.update(
+        { _id: kid.id },
+        { acuarelausers: [parents[0].id, parents[1].id] }
+      );
       return ctx.send({
         ok: true,
         status: 200,
         code: 1,
         kid: kidEdited,
-        parents
+        parents,
       });
-
     } else return ctx.send(validToken);
   },
 };

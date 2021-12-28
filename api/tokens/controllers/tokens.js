bf42bfc0b8ff20fa4d2d4aaa56d857aadfa55e97
jdda95 <j.delgado@orekaconsultores.com>
2021-12-28 12:36:01 -0500

    Token generation

--- api/tokens/controllers/tokens.js
+++ api/tokens/controllers/tokens.js
@@ -1,8 +1,33 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create: async (ctx) => {
+    const { email } = ctx.params;
+    var chars =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var token = "";
+    for (var i = 0; i < 30; i++) {
+      token += chars[Math.floor(Math.random() * chars.length)];
+    }
+    const tokencreated = await strapi.services.tokens.create({ token: token });
+    const user = await strapi.services.acuarelauser.findOne({ email });
+    if (user) {
+      return strapi.services.acuarelauser.update(
+        { id: user.id },
+        { token: tokencreated.id }
+      );
+    } else {
+      return ctx.send(
+        {
+          msg: "There is no user with this email",
+        },
+        409
+      );
+    }
+  },
+};

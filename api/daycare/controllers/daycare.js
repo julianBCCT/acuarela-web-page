af0d215c2c7c91695e035026e3221a60986207f6
jdda95 <j.delgado@orekaconsultores.com>
2022-08-04 13:11:08 -0500

    asd

--- api/daycare/controllers/daycare.js
+++ api/daycare/controllers/daycare.js
@@ -10,34 +10,6 @@ const verification = require('../../../middlewares/authJwt');
  */
 
 module.exports = {
-  // Lista los daycares y el usuario asociado a ellos
-  async find(ctx) {
-    const { token } = ctx.request.header;
-    let validToken = await verification.renew(token);
-
-    if (validToken.ok) {
-      let query = { active: true };
-      query.acuarelausers  = { $eq: validToken.user.id };
-      console.log("🚀 ~ file: daycare.js ~ line 21 ~ find ~ query", query)
-
-      let entity = await strapi.query('daycare').model.find(query);
-      console.log("🚀 ~ file: daycare.js ~ line 23 ~ find ~ entity", entity)
-
-      if (!entity)
-        return ctx.send({
-          ok: false,
-          status: 404,
-          code: 5,
-          msg: 'Daycare not found.',
-        });
-      else {
-        validToken.msg = 'Query completed successfully!';
-        validToken.response = entity;
-        return ctx.send(validToken);
-      }
-    } else return ctx.send(validToken);
-  },
-
   async findOne(ctx) {
     const { id } = ctx.params;
     const { token } = ctx.request.header;

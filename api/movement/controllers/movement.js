a7212d87622ebaef651b37fe3907d12d11eae2e7
jdda95 <j.delgado@orekaconsultores.com>
2022-08-19 10:10:10 -0500

    movement

--- api/movement/controllers/movement.js
+++ api/movement/controllers/movement.js
@@ -8,33 +8,6 @@ const { sanitizeEntity } = require("strapi-utils");
  */
 
 module.exports = {
-  async find(ctx) {
-    const { token } = ctx.request.header;
-    let validToken = await verification.renew(token);
-
-    if (validToken.ok) {
-      let query = { ...ctx.query };
-      query.daycare = { $eq: validToken.user.organization.id };
-      const { response } = ctx.request.body;
-      let entity = await strapi
-        .query("movement")
-        .model.find(query)
-        .populate("payer", ["name", "lastname", "mail", "phone"]);
-
-      if (!entity)
-        return ctx.send({
-          ok: true,
-          status: 200,
-          code: 0,
-          msg: "movements not found.",
-        });
-      else {
-        response.msg = "Query completed successfully!";
-        response.response = entity;
-        return ctx.send(response);
-      }
-    }
-  },
   async findByUser(params, populate) {
     return strapi.query("movement").find(params, populate);
   },

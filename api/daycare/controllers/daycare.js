9934af6523ce331882b0a3a88d4d6319fcad6232
jdda95 <j.delgado@orekaconsultores.com>
2022-09-27 09:41:17 -0500

    single daycare

--- api/daycare/controllers/daycare.js
+++ api/daycare/controllers/daycare.js
@@ -1,8 +1,8 @@
-'use strict';
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const verification = require('../../../middlewares/authJwt');
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const verification = require("../../../middlewares/authJwt");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
@@ -12,29 +12,24 @@ const verification = require('../../../middlewares/authJwt');
 module.exports = {
   async findOne(ctx) {
     const { id } = ctx.params;
-    const { token } = ctx.request.header;
 
-    let validToken = await verification.renew(token);
+    let query = { active: true };
+    query._id = { $eq: id };
 
-    if (validToken.ok) {
-      let query = { active: true };
-      query._id = { $eq: id };
-
-      // Se realiza la consulta sobre un niño y se poblan los campos necesarios.
-      let entity = await strapi.query('daycare').model.find(query);
-      //.populate('activities');
-      if (!entity)
-        return ctx.send({
-          ok: false,
-          status: 404,
-          code: 5,
-          msg: 'Daycare not found.',
-        });
-      else {
-        validToken.msg = 'Query completed successfully!';
-        validToken.response = entity;
-        return ctx.send(validToken);
-      }
-    } else return ctx.send(validToken);
+    // Se realiza la consulta sobre un niño y se poblan los campos necesarios.
+    let entity = await strapi.query("daycare").model.find(query);
+    //.populate('activities');
+    if (!entity)
+      return ctx.send({
+        ok: false,
+        status: 404,
+        code: 5,
+        msg: "Daycare not found.",
+      });
+    else {
+      validToken.msg = "Query completed successfully!";
+      validToken.response = entity;
+      return ctx.send(validToken);
+    }
   },
 };

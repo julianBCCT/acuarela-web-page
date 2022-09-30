5117910ebda2843dae79a59e72045997c520d3da
Julian Delgado <dreinovcorp@gmail.com>
2022-09-30 17:05:02 -0500

    Bilingual user validate

--- api/bilingual-user/controllers/bilingual-user.js
+++ api/bilingual-user/controllers/bilingual-user.js
@@ -5,4 +5,19 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    async create(){
+        let user = ctx.request.body;
+        let entity;
+        entity = await strapi.services.bilingual_users.findOne({email: user.email});
+        if (!entity || entity == []) {
+            entity = await strapi.services.bilingual_users.create(user);
+        }else {
+            let msg = 'Ya existe un usuario registrado con este correo';
+            let code = 'e-2';
+            return ctx.send({ ok: false, status: 400, code, msg });
+        }
+        return ctx.send({ respuesta });
+    }
+};

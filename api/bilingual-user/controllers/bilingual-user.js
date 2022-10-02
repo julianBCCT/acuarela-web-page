2b4e8314f31a8f3afd0bc13b0d9b6279f2192444
Julian Delgado <dreinovcorp@gmail.com>
2022-10-02 00:57:03 -0500

    User Login

--- api/bilingual-user/controllers/bilingual-user.js
+++ api/bilingual-user/controllers/bilingual-user.js
@@ -12,12 +12,26 @@ module.exports = {
         let entity;
         entity = await strapi.services.bilingual_users.findOne({email: user.email});
         if (!entity || entity == []) {
+            const hashedPassword = await bcrypt.hash(user.password, 10);
+            user.password = hashedPassword;
             entity = await strapi.services.bilingual_users.create(user);
+            return ctx.send({ ok: true, status: 200, entity });
         }else {
             let msg = 'Ya existe un usuario registrado con este correo';
             let code = 'e-2';
             return ctx.send({ ok: false, status: 400, code, msg });
         }
-        return ctx.send({ respuesta });
-    }
+    },
+    async login(ctx) {
+        const { email, password } = ctx.request.body;
+        let entity;
+        entity = await strapi.services.acuarelauser.findOne({ email });
+        if (entity) {
+            let result = await bcrypt.compare(password, entity.password);
+            return ctx.send(result);
+        }else {
+            return ctx.send({ ok: false, status: 400, msg:"No se encontro un usuario registrado con este correo." });
+        }
+
+      },
 };

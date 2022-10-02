fe977b2ca3dd4550a410d74298a237089c098c44
Julian Delgado <dreinovcorp@gmail.com>
2022-10-02 01:21:36 -0500

    const bcrypt = require('bcryptjs');

--- api/bilingual-user/controllers/bilingual-user.js
+++ api/bilingual-user/controllers/bilingual-user.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const bcrypt = require('bcryptjs');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller

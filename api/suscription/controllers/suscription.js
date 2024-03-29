43d427bde6bacc86da9b2e4fcea947449e7c8ed0
Oblicua <administracion@oblicua.co>
2024-03-28 23:42:53 -0500

    Expired subscriptions retrieved

--- api/suscription/controllers/suscription.js
+++ api/suscription/controllers/suscription.js
@@ -1,8 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findExpired(ctx) {
+    try {
+      const currentDate = new Date();
+
+      // Consultar suscripciones vencidas
+      const expiredSubscriptions = await strapi.query("suscriptions").find({
+        suscription_expiration: { $lt: currentDate },
+      });
+
+      return ctx.send({
+        ok: true,
+        status: 200,
+        code: 0,
+        msg: "Expired subscriptions retrieved successfully!",
+        response: expiredSubscriptions,
+      });
+    } catch (error) {
+      console.error("Error in findExpired method:", error);
+      return ctx.send({
+        ok: false,
+        status: 500,
+        code: 10,
+        msg: "Internal server error.",
+      });
+    }
+  },
+};

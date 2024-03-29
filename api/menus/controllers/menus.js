4193441010571eac59fb66e06fcb77acd911b42d
Oblicua <administracion@oblicua.co>
2024-03-29 00:16:06 -0500

    version_promp

--- api/menus/controllers/menus.js
+++ api/menus/controllers/menus.js
@@ -3,10 +3,12 @@ const _ = require("lodash");
 
 module.exports = {
   async findOneRandom(ctx) {
-    const { age_group } = ctx.params;
+    const { age_group, version_promp } = ctx.params;
 
     // Consulta para encontrar todos los menús filtrados por el campo especificado
-    let entities = await strapi.query("menus").find({ age_group });
+    let entities = await strapi
+      .query("menus")
+      .find({ age_group, version_promp });
 
     // Verificar si hay menús disponibles para el grupo de edad dado
     if (entities.length === 0) {

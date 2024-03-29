2a1c77b4cf53fa259c67ec5e722a056863577cf1
Oblicua <administracion@oblicua.co>
2024-03-28 23:26:06 -0500

    RandomMenu

--- api/menus/controllers/menus.js
+++ api/menus/controllers/menus.js
@@ -1,8 +1,32 @@
-'use strict';
+// En tu controlador, por ejemplo, `menu.js`
+const _ = require("lodash");
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async findOneRandom(ctx) {
+    const { age_group } = ctx.params;
 
-module.exports = {};
+    // Consulta para encontrar todos los menús filtrados por el campo especificado
+    let entities = await strapi.query("menus").find({ age_group });
+
+    // Verificar si hay menús disponibles para el grupo de edad dado
+    if (entities.length === 0) {
+      return ctx.send({
+        ok: false,
+        status: 404,
+        code: 5,
+        msg: `No menu found for age group: ${age_group}`,
+      });
+    }
+
+    // Seleccionar un menú aleatorio de la lista
+    const randomMenu = _.sample(entities);
+
+    return ctx.send({
+      ok: true,
+      status: 200,
+      code: 0,
+      msg: "Random menu retrieved successfully!",
+      response: randomMenu,
+    });
+  },
+};

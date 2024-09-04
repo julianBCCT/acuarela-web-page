9a433adeb0902f6d1739c256daefac8d44d4b28a
Oblicua <administracion@oblicua.co>
2024-09-04 12:24:46 -0500

    chat sendMessages

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,44 +10,58 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports =  () => {
-  const io = require('socket.io')(strapi.server, {
-      cors: {
-          origin: [
-              "http://localhost:3000",
-              "http://localhost:1337",
-              "http://localhost:5500",
-              "http://localhost:5501",
-              "http://198.211.114.51",
-              "chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop",
-              "https://acuarelacore.com",
-              "https://acuarela.app",
-              "http://acuarela.app",
-              "https://acuarelacore.com/api/movements/62d1d62c2d088187dac74153",
-              "https://bilingualchildcaretraining.com",
-              "https://dev.bilingualchildcaretraining.com",
-          ],
-          methods: ["GET", "POST"],
-          credentials: true
-      }
+module.exports = () => {
+  const io = require("socket.io")(strapi.server, {
+    cors: {
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:1337",
+        "http://localhost:5500",
+        "http://localhost:5501",
+        "http://198.211.114.51",
+        "chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop",
+        "https://acuarelacore.com",
+        "https://acuarela.app",
+        "http://acuarela.app",
+        "https://acuarelacore.com/api/movements/62d1d62c2d088187dac74153",
+        "https://bilingualchildcaretraining.com",
+        "https://dev.bilingualchildcaretraining.com",
+      ],
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
   });
 
-  io.on('connection', (socket) => {
-      console.log('A user connected', socket.id);
-      
-      socket.on('join', ({ username, room }) => {
-          console.log("user connected");
-          console.log("username is ", username);
-          console.log("room is...", room);
-          // Emit an acknowledgment back to the client
-          socket.emit('joined', { message: `Welcome ${username} to room ${room}`, socketId: socket.id });
-      });
+  io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
 
-      socket.on('disconnect', () => {
-          console.log('User disconnected', socket.id);
+    socket.on("join", ({ username, room }) => {
+      console.log("user connected");
+      console.log("username is ", username);
+      console.log("room is...", room);
+      // Emit an acknowledgment back to the client
+      socket.emit("joined", {
+        message: `Welcome ${username} to room ${room}`,
+        socketId: socket.id,
       });
+    });
+
+    socket.on("sendMessage", async (data) => {
+      let strapiData = {
+        user: data.user,
+        message: data.message,
+      };
+      await strapi.services.chats.create(strapiData);
+      socket.broadcast.to("group").emit("message", {
+        user: data.username,
+        text: data.message,
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected", socket.id);
+    });
   });
 
   strapi.io = io; // Make it available globally if needed
 };
-

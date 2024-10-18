b6a9f7ac74fd330a3bc4a2bf1cd654d44bd30832
Bogotadctravel <nestor.goyes@idt.gov.co>
2024-10-18 16:28:20 -0500

    Estudiantes customSearch

--- api/estudiantes/controllers/estudiantes.js
+++ api/estudiantes/controllers/estudiantes.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async customSearch(ctx) {
+    const { nombre_contains } = ctx.query;
+
+    if (!nombre_contains) {
+      return ctx.badRequest("Debe proporcionar un valor para nombre_contains");
+    }
+
+    try {
+      const estudiantes = await strapi.query("estudiantes").find({
+        nombre_contains,
+      });
+
+      return estudiantes.map((estudiante) =>
+        sanitizeEntity(estudiante, { model: strapi.models.estudiantes })
+      );
+    } catch (error) {
+      return ctx.badRequest("Error al buscar estudiantes", error);
+    }
+  },
+};

b64f02ce677d40d84b534f263eb738749ef53218
Bogotadctravel <nestor.goyes@idt.gov.co>
2024-11-06 12:04:54 -0500

    test createMultipleAsistencias

--- api/asistencia-cda/controllers/asistencia-cda.js
+++ api/asistencia-cda/controllers/asistencia-cda.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async createMultipleAsistencias(ctx) {
+    const body = ctx.request.body;
+    // const entity = await strapi.services.acuarelauser.findOne({ id });
+    return body;
+  },
+};

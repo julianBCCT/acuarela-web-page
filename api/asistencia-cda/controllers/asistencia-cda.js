ac8da98b527ddf50eab608bdf818ee0afe9699dc
Bogotadctravel <nestor.goyes@idt.gov.co>
2024-11-06 12:49:09 -0500

    s

--- api/asistencia-cda/controllers/asistencia-cda.js
+++ api/asistencia-cda/controllers/asistencia-cda.js
@@ -9,39 +9,61 @@ const moment = require("moment");
 
 module.exports = {
   async createMultipleAsistencias(ctx) {
-    // Recibir datos del BODY
-    const { participants } = ctx.request.body;
-    // Filtrar el arreglo de participantes para solo devolver el displayName
-    let AllParticipants = participants.map((participant) => {
-      let {
-        signedinUser: { displayName },
-      } = participant;
-      return displayName;
-    });
-    // Fecha que trae 1 de lso estudiantes
-    let dateStr = participants.map(
-      (participant) => participant.earliestStartTime
-    );
-    // Formatear fecha YYYY-MM-DD
-    const date = new Date(dateStr);
-    // Ajustar la hora a las 00:00:00
-    date.setUTCHours(0, 0, 0, 0);
-    // Convertir la fecha a ISO y recortar el tiempo para dejar solo "00:00:00"
-    const formattedDate = date.toISOString().split("T")[0] + "T00:00:00";
-    let formatDate = moment(formattedDate).format("YYYY-MM-DD");
-    // Traer todos los estudiantes
-    let allEstudiantes = await strapi.query("estudiantes").model.find();
-    // Filtrar estudiantes por Nombre comparando los 2 arreglos allEstudiantes - AllParticipants
-    const filteredEstudiantes = allEstudiantes.filter((estudiante) =>
-      AllParticipants.some((participant) => participant === estudiante.nombre)
-    );
+    try {
+      // Recibir datos del BODY
+      const { participants } = ctx.request.body;
+      // Filtrar el arreglo de participantes para solo devolver el displayName
+      let AllParticipants = participants.map((participant) => {
+        let {
+          signedinUser: { displayName },
+        } = participant;
+        return displayName;
+      });
+      // Fecha que trae 1 de lso estudiantes
+      let earliestStartTimedateStr = participants.map(
+        (participant) => participant.earliestStartTime
+      );
+      let latestEndTime = participants.map(
+        (participant) => participant.latestEndTime
+      );
+      // Formatear fecha YYYY-MM-DD
+      const date = new Date(earliestStartTimedateStr);
+      // Ajustar la hora a las 00:00:00
+      date.setUTCHours(0, 0, 0, 0);
+      // Convertir la fecha a ISO y recortar el tiempo para dejar solo "00:00:00"
+      const formattedDate = date.toISOString().split("T")[0] + "T00:00:00";
+      let formatDate = moment(formattedDate).format("YYYY-MM-DD");
+      // Traer todos los estudiantes
+      let allEstudiantes = await strapi.query("estudiantes").model.find();
+      // Filtrar estudiantes por Nombre comparando los 2 arreglos allEstudiantes - AllParticipants
+      const filteredEstudiantes = allEstudiantes.filter((estudiante) =>
+        AllParticipants.some((participant) => participant === estudiante.nombre)
+      );
+      let query = {};
+      query.Fecha = { $eq: formatDate };
+      let clase = await strapi.query("classes").model.find(query);
 
-    return {
-      AllParticipants,
-      formatDate,
-      date,
-      allEstudiantes,
-      filteredEstudiantes,
-    };
+      let asistencias = await Promise.all(
+        filteredEstudiantes.map(async (estudiante) => {
+          let asistencia = await strapi.services.classes.create({
+            class: clase.id,
+            estudiante: estudiante.id,
+            nombre: estudiante.nombre,
+            email: estudiante.email,
+            hora_ingreso: earliestStartTimedateStr,
+            hora_salida: latestEndTime,
+          });
+          return asistencia;
+        })
+      );
+
+      return {
+        filteredEstudiantes,
+        clase,
+        asistencias, // Todas las asistencias creadas
+      };
+    } catch (err) {
+      return ctx.badRequest("Error while creating entries", err.message);
+    }
   },
 };

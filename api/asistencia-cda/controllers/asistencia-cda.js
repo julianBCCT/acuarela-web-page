6762a393c318d5983266b9bb5aaa9cda75b28469
Bogotadctravel <nestor.goyes@idt.gov.co>
2024-11-06 12:35:40 -0500

    s

--- api/asistencia-cda/controllers/asistencia-cda.js
+++ api/asistencia-cda/controllers/asistencia-cda.js
@@ -9,18 +9,29 @@ const moment = require("moment");
 
 module.exports = {
   async createMultipleAsistencias(ctx) {
-    const { participants, fecha } = ctx.request.body;
-
+    // Recibir datos del BODY
+    const { participants } = ctx.request.body;
+    // Filtrar el arreglo de participantes para solo devolver el displayName
     let AllParticipants = participants.map((participant) => {
       let {
         signedinUser: { displayName },
       } = participant;
       return displayName;
     });
-    let date = participants.map((participant) => participant.earliestStartTime);
-    let formatDate = moment(date).format("YYYY-MM-DD");
-    // const entity = await strapi.services.acuarelauser.findOne({ id });
+    // Fecha que trae 1 de lso estudiantes
+    let dateStr = participants.map(
+      (participant) => participant.earliestStartTime
+    );
+    // Formatear fecha YYYY-MM-DD
+    const date = new Date(dateStr);
+    // Ajustar la hora a las 00:00:00
+    date.setUTCHours(0, 0, 0, 0);
+    // Convertir la fecha a ISO y recortar el tiempo para dejar solo "00:00:00"
+    const formattedDate = date.toISOString().split("T")[0] + "T00:00:00";
+    let formatDate = moment(formattedDate).format("YYYY-MM-DD");
+    // Traer todos los estudiantes
     let allEstudiantes = await strapi.query("estudiantes").model.find();
+    // Filtrar estudiantes por Nombre comparando los 2 arreglos allEstudiantes - AllParticipants
     const filteredEstudiantes = allEstudiantes.filter((estudiante) =>
       AllParticipants.some((participant) => participant === estudiante.nombre)
     );

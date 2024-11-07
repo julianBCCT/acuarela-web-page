4b2880c35758fcb42b0bff86be072ae3dd07041e
Bogotadctravel <nestor.goyes@idt.gov.co>
2024-11-07 09:27:10 -0500

    customSearch

--- api/asistencia-cda/controllers/asistencia-cda.js
+++ api/asistencia-cda/controllers/asistencia-cda.js
@@ -83,7 +83,6 @@ module.exports = {
     }
   },
   async createMultipleAsistenciasTest(ctx) {
-    // Función de normalización de nombres
     const normalizeName = (name) => {
       return name
         .trim()
@@ -91,24 +90,22 @@ module.exports = {
         .normalize("NFD") // Descomponer caracteres con tildes
         .replace(/[\u0300-\u036f]/g, ""); // Eliminar los diacríticos (acentos)
     };
+
     try {
       const { participants } = ctx.request.body;
 
-      // Filtrar el arreglo de participantes para solo devolver el displayName
-      let AllParticipants = participants.map((participant) => {
-        if (participant.signedinUser) {
-          if (participant.signedinUser.displayName) {
-            let {
-              signedinUser: { displayName },
-            } = participant;
-            return normalizeName(displayName); // Normalizar nombres de los participantes
-          } else {
-            return "";
+      // Filtrar y normalizar los nombres de los participantes
+      let AllParticipants = participants
+        .map((participant) => {
+          if (
+            participant.signedinUser &&
+            participant.signedinUser.displayName
+          ) {
+            return normalizeName(participant.signedinUser.displayName);
           }
-        } else {
           return "";
-        }
-      });
+        })
+        .filter(Boolean); // Filtrar valores vacíos
 
       // Extraer las fechas de earliestStartTime y latestEndTime
       let earliestStartTimes = participants.map(
@@ -118,7 +115,6 @@ module.exports = {
         (participant) => participant.latestEndTime
       );
 
-      // Verificar si las fechas son válidas
       const earliestStartTime = new Date(earliestStartTimes[0]);
       const latestEndTime = new Date(latestEndTimes[0]);
 
@@ -129,35 +125,38 @@ module.exports = {
         throw new Error("Invalid time value");
       }
 
-      // Formatear la fecha en el formato deseado
       const formatDate = moment(earliestStartTime).format("YYYY-MM-DD");
 
-      // Obtener todos los estudiantes
-      let allEstudiantes = await strapi.query("estudiantes").model.find();
+      // Llamar a la función customSearch para obtener los estudiantes filtrados
+      let filteredEstudiantesPromises = AllParticipants.map(async (name) => {
+        const estudiantes = await strapi.controllers.estudiantes.customSearch({
+          query: { nombre_contains: name },
+        });
+        return estudiantes.length > 0 ? estudiantes[0] : null; // Asumimos que queremos el primer resultado si hay coincidencias
+      });
 
-      // Filtrar estudiantes por nombre, normalizando también el nombre del estudiante
-      const filteredEstudiantes = allEstudiantes.filter(
-        (estudiante) =>
-          AllParticipants.includes(normalizeName(estudiante.nombre)) // Normalizar nombres de estudiantes
-      );
+      // Obtener los resultados de todas las promesas
+      let filteredEstudiantes = (
+        await Promise.all(filteredEstudiantesPromises)
+      ).filter(Boolean);
 
       let query = { Fecha: { $eq: formatDate } };
       let clase = await strapi.query("classes").model.findOne(query);
 
       // Crear asistencias para los estudiantes filtrados
-      // let asistencias = await Promise.all(
-      //   filteredEstudiantes.map(async (estudiante) => {
-      //     let asistencia = await strapi.services["asistencia-cda"].create({
-      //       class: clase.id,
-      //       estudiante: estudiante.id,
-      //       nombre: estudiante.nombre,
-      //       email: estudiante.email,
-      //       hora_ingreso: earliestStartTime.toISOString(),
-      //       hora_salida: latestEndTime.toISOString(),
-      //     });
-      //     return asistencia;
-      //   })
-      // );
+      let asistencias = await Promise.all(
+        filteredEstudiantes.map(async (estudiante) => {
+          let asistencia = await strapi.services["asistencia-cda"].create({
+            class: clase.id,
+            estudiante: estudiante.id,
+            nombre: estudiante.nombre,
+            email: estudiante.email,
+            hora_ingreso: earliestStartTime.toISOString(),
+            hora_salida: latestEndTime.toISOString(),
+          });
+          return asistencia;
+        })
+      );
 
       return {
         clase,

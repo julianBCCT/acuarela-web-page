15695d376fcb0568ca2089643b9395e3e5e9a73a
Bogotadctravel <nestor.goyes@idt.gov.co>
2024-11-07 08:49:37 -0500

    test asistencias

--- api/asistencia-cda/controllers/asistencia-cda.js
+++ api/asistencia-cda/controllers/asistencia-cda.js
@@ -59,11 +59,6 @@ module.exports = {
           AllParticipants.includes(normalizeName(estudiante.nombre)) // Normalizar nombres de estudiantes
       );
 
-      // Verificar si se encuentran estudiantes coincidentes
-      if (filteredEstudiantes.length === 0) {
-        throw new Error("No students found for the provided participants");
-      }
-
       let query = { Fecha: { $eq: formatDate } };
       let clase = await strapi.query("classes").model.findOne(query);
 
@@ -83,6 +78,80 @@ module.exports = {
       );
 
       return asistencias;
+    } catch (err) {
+      return ctx.badRequest("Error while creating entries", err.message);
+    }
+  },
+  async createMultipleAsistenciasTest(ctx) {
+    // Función de normalización de nombres
+    const normalizeName = (name) => {
+      return name
+        .trim()
+        .toLowerCase()
+        .normalize("NFD") // Descomponer caracteres con tildes
+        .replace(/[\u0300-\u036f]/g, ""); // Eliminar los diacríticos (acentos)
+    };
+    try {
+      const { participants } = ctx.request.body;
+
+      // Filtrar el arreglo de participantes para solo devolver el displayName
+      let AllParticipants = participants.map((participant) => {
+        let {
+          signedinUser: { displayName },
+        } = participant;
+        return normalizeName(displayName); // Normalizar nombres de los participantes
+      });
+
+      // Extraer las fechas de earliestStartTime y latestEndTime
+      let earliestStartTimes = participants.map(
+        (participant) => participant.earliestStartTime
+      );
+      let latestEndTimes = participants.map(
+        (participant) => participant.latestEndTime
+      );
+
+      // Verificar si las fechas son válidas
+      const earliestStartTime = new Date(earliestStartTimes[0]);
+      const latestEndTime = new Date(latestEndTimes[0]);
+
+      if (
+        isNaN(earliestStartTime.getTime()) ||
+        isNaN(latestEndTime.getTime())
+      ) {
+        throw new Error("Invalid time value");
+      }
+
+      // Formatear la fecha en el formato deseado
+      const formatDate = moment(earliestStartTime).format("YYYY-MM-DD");
+
+      // Obtener todos los estudiantes
+      let allEstudiantes = await strapi.query("estudiantes").model.find();
+
+      // Filtrar estudiantes por nombre, normalizando también el nombre del estudiante
+      const filteredEstudiantes = allEstudiantes.filter(
+        (estudiante) =>
+          AllParticipants.includes(normalizeName(estudiante.nombre)) // Normalizar nombres de estudiantes
+      );
+
+      let query = { Fecha: { $eq: formatDate } };
+      let clase = await strapi.query("classes").model.findOne(query);
+
+      // Crear asistencias para los estudiantes filtrados
+      // let asistencias = await Promise.all(
+      //   filteredEstudiantes.map(async (estudiante) => {
+      //     let asistencia = await strapi.services["asistencia-cda"].create({
+      //       class: clase.id,
+      //       estudiante: estudiante.id,
+      //       nombre: estudiante.nombre,
+      //       email: estudiante.email,
+      //       hora_ingreso: earliestStartTime.toISOString(),
+      //       hora_salida: latestEndTime.toISOString(),
+      //     });
+      //     return asistencia;
+      //   })
+      // );
+
+      return { clase, filteredEstudiantes, AllParticipants };
     } catch (err) {
       return ctx.badRequest("Error while creating entries", err.message);
     }

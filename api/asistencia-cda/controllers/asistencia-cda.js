e090548c7a08bf9db6ab06cc013da6ccfc6c8fc7
Bogotadctravel <nestor.goyes@idt.gov.co>
2024-11-19 16:14:34 -0500

    updateRegisters CDA

--- api/asistencia-cda/controllers/asistencia-cda.js
+++ api/asistencia-cda/controllers/asistencia-cda.js
@@ -195,4 +195,90 @@ module.exports = {
       });
     }
   },
+  async updateMultipleAsistencias(ctx) {
+    const normalizeName = (name) => {
+      return name
+        .trim()
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+    };
+
+    try {
+      const {
+        conference: {
+          startTime,
+          endTime,
+          asistentes: { participants },
+        },
+      } = ctx.request.body;
+
+      // Validar entradas
+      if (!Array.isArray(participants) || participants.length === 0) {
+        throw new Error("participants no es un array válido o está vacío");
+      }
+      if (!startTime || typeof startTime !== "string") {
+        throw new Error("startTime no es un string válido");
+      }
+      if (!endTime || typeof endTime !== "string") {
+        throw new Error("endTime no es un string válido");
+      }
+
+      // Normalizar nombres
+      let allParticipants = participants
+        .map((participant) =>
+          participant.signedinUser
+            ? normalizeName(participant.signedinUser.displayName)
+            : normalizeName(participant.anonymousUser.displayName || "")
+        )
+        .filter(Boolean);
+
+      // Manejo de fechas
+      const earliestStartTime = moment(
+        startTime,
+        moment.ISO_8601,
+        true
+      ).toDate();
+      const latestEndTime = moment(endTime, moment.ISO_8601, true).toDate();
+
+      if (
+        isNaN(earliestStartTime.getTime()) ||
+        isNaN(latestEndTime.getTime())
+      ) {
+        throw new Error("Formato de fecha no válido");
+      }
+
+      // Buscar estudiantes
+      let filteredEstudiantes = await Promise.all(
+        allParticipants.map(async (name) => {
+          try {
+            const estudiantes =
+              await strapi.controllers.estudiantes.customSearch({
+                query: { nombre_contains: name },
+              });
+            return estudiantes.length > 0 ? estudiantes[0] : null;
+          } catch (err) {
+            console.error(`Error buscando estudiante: ${name}`, err);
+            return null;
+          }
+        })
+      ).then((res) => res.filter(Boolean));
+
+      // Buscar clase
+      let clase = await strapi.query("classes").model.findOne({
+        Fecha: { $eq: "2024-11-17" },
+      });
+
+      return {
+        today: moment("", moment.ISO_8601, true).toDate(),
+        clase,
+        filteredEstudiantes: filteredEstudiantes.map((est) => est.nombre),
+      };
+    } catch (err) {
+      return ctx.badRequest("Error while creating entries", {
+        message: err.message,
+        stack: err.stack,
+      });
+    }
+  },
 };

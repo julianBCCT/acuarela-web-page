c7f9775edf72beb953e54a77fae4520ef86c29ca
Julián Delgado <nestor@oblicua.co>
2025-01-02 09:35:46 -0500

    Test Cronjobs

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,21 +1,12 @@
 'use strict';
 
-/**
- * Cron config that gives you an opportunity
- * to run scheduled jobs.
- *
- * The cron format consists of:
- * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
- *
- * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#cron-tasks
- */
-
 module.exports = {
-  /**
-   * Simple example.
-   * Every monday at 1am.
-   */
-  // '0 1 * * 1': () => {
-  //
-  // }
+  // Run every minute
+  '* * * * *': async () => {
+    strapi.log.info('Cron job executed');
+    
+    // Perform some test actions
+    // Example: log the current time
+    strapi.log.info(`Current time: ${new Date().toISOString()}`);
+  },
 };
--- config/server.js
+++ config/server.js
@@ -4,6 +4,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
   url: env('API'),
+  cron: {
+    enabled: true,
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '06b77711f7a956f6477095a04d7034de'),

26dff0c89cee9166fd5be0c112165a750498499b
Julián Delgado <nestor@oblicua.co>
2025-02-23 18:00:40 -0500

    inscripciones validacion completeInsc

--- api/inscripciones/controllers/inscripciones.js
+++ api/inscripciones/controllers/inscripciones.js
@@ -10,68 +10,15 @@ const verification = require("../../../middlewares/authJwt");
 
 module.exports = {
   async completeInsc(ctx) {
-    const { token } = ctx.request.header;
-    const child = ctx.request.body;
-    let validToken = await verification.renew(token);
-    if (validToken.ok) {
-      child.status = true;
-      child.attitudes = [];
-      const kid = await strapi.services.children.create(child);
-      const hashedPassword = await bcrypt.hash("123456", 10);
-      if (child.parents_rel) {
-        if (child.parents_rel.length > 0) {
-          const kidEdited = await strapi.services.children.update(
-            { _id: kid.id },
-            {
-              acuarelausers: child.parents_rel,
-            }
-          );
-          return ctx.send(
-            {
-              ok: true,
-              status: 200,
-              code: 1,
-              kid: kidEdited,
-            },
-            200
-          );
-        }
-      } else {
-        let parents = [];
-        for (const parent of child.parents) {
-          parent.password = hashedPassword;
-          parent.status = true;
-          parent.rols = ["5ff790045d6f2e272cfd7394"];
-          parent.children = [kid.id];
-          parent.mail = parent.email;
-          parent.daycare = child.daycare;
-          if (parent.name != "") {
-            let entity = await strapi.services.acuarelauser.create(parent);
-            parents.push(entity);
-          }
-        }
-
-        const kidEdited = await strapi.services.children.update(
-          { _id: kid.id },
-          {
-            acuarelausers: parents.map((parent) => parent.id),
-          }
-        );
-        return ctx.send(
-          {
-            ok: true,
-            status: 200,
-            code: 1,
-            kid: kidEdited,
-            parents,
-          },
-          200
-        );
-      }
-    } else return ctx.send(validToken);
+    return ctx.send(
+      {
+        ok: true,
+        status: 200,
+        code: 1,
+      },
+      200
+    );
   },
-
-  
   async findByPaymentTime(ctx) {
     const { status, 'payment.time': paymentTime } = ctx.query;
   
@@ -195,9 +142,8 @@ module.exports = {
       mensual: categorizeEntities("Mensual").map((entity) => entity.child.movements),
       diario: categorizeEntities("Diario").map((entity) => entity.child.movements),
     };
-  }
-,
-    async checkAndNotify(ctx) {
+  },
+  async checkAndNotify(ctx) {
       try {
         const filters = {};
         filters.status = "Finalizado";

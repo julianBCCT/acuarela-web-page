774fa46eda5583f300c35d21efacbdaff6c0608e
Bogotadctravel <nestor.goyes@idt.gov.co>
2025-03-11 20:18:33 -0500

    inscription

--- api/inscripciones/controllers/inscripciones.js
+++ api/inscripciones/controllers/inscripciones.js
@@ -1,6 +1,6 @@
 "use strict";
 const { sanitizeEntity } = require("strapi-utils");
-const FormData = require('form-data');
+const FormData = require("form-data");
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
@@ -11,7 +11,6 @@ const generateRandomCode = () => {
   return Math.floor(100000 + Math.random() * 900000); // Número aleatorio de 6 dígitos
 };
 
-
 module.exports = {
   async completeInsc(ctx) {
     const { token } = ctx.request.header;
@@ -19,110 +18,113 @@ module.exports = {
     let validToken = await verification.renew(token);
 
     if (validToken.ok) {
-        child.status = true;
-        child.attitudes = [];
-        const kid = await strapi.services.children.create(child);
-        const hashedPassword = await bcrypt.hash("123456", 10);
-        let parents = [];
+      child.status = true;
+      child.attitudes = [];
+      const kid = await strapi.services.children.create(child);
+      const hashedPassword = await bcrypt.hash("123456", 10);
+      let parents = [];
 
-        for (const parent of child.parents) {
-            if (parent.name !== "") {
-                parent.status = false;
-                parent.mail = parent.email;
-                parent.daycare = child.daycare;
+      for (const parent of child.parents) {
+        if (parent.name !== "") {
+          parent.status = false;
+          parent.mail = parent.email;
+          parent.daycare = child.daycare;
 
-                // Buscar si ya existe un usuario con ese email
-                let existingUser = await strapi.services.acuarelauser.findOne({ email: parent.email });
+          // Buscar si ya existe un usuario con ese email
+          let existingUser = await strapi.services.acuarelauser.findOne({
+            email: parent.email,
+          });
 
-                if (existingUser) {
-                    // No actualizar la contraseña si ya existe
-                    const { password, rols, children, ...parentData } = parent;
+          if (existingUser) {
+            // No actualizar la contraseña si ya existe
+            const { password, rols, children, ...parentData } = parent;
 
-                    // Mantener los roles existentes y agregar el nuevo solo si no está
-                    const newRol = "5ff790045d6f2e272cfd7394";
-                    const updatedRols = existingUser.rols.includes(newRol) 
-                        ? existingUser.rols 
-                        : [...existingUser.rols, newRol];
+            // Mantener los roles existentes y agregar el nuevo solo si no está
+            const newRol = "5ff790045d6f2e272cfd7394";
+            const updatedRols = existingUser.rols.includes(newRol)
+              ? existingUser.rols
+              : [...existingUser.rols, newRol];
 
-                    // Mantener los children existentes y agregar el nuevo solo si no está
-                    const updatedChildren = existingUser.children.includes(kid.id)
-                        ? existingUser.children
-                        : [...existingUser.children, kid.id];
+            // Mantener los children existentes y agregar el nuevo solo si no está
+            const updatedChildren = existingUser.children.includes(kid.id)
+              ? existingUser.children
+              : [...existingUser.children, kid.id];
 
-                    let updatedUser = await strapi.services.acuarelauser.update(
-                        { id: existingUser.id },
-                        { ...parentData, rols: updatedRols, children: updatedChildren }
-                    );
-                    parents.push(updatedUser);
-                } else {
-                    // Crear código dinámico y asignarlo al nuevo usuario
-                    let code = await strapi.services["codigo-dinamico"].create({ Codigo: generateRandomCode() });
-                    parent.codigo_dinamico = code.id;
-                    parent.password = hashedPassword; // Solo asignar password al crear
-                    parent.rols = ["5ff790045d6f2e272cfd7394"]; // Asignar rol solo al crear
-                    parent.children = [kid.id]; // Asignar primer hijo solo al crear
+            let updatedUser = await strapi.services.acuarelauser.update(
+              { id: existingUser.id },
+              { ...parentData, rols: updatedRols, children: updatedChildren }
+            );
+            parents.push(updatedUser);
+          } else {
+            // Crear código dinámico y asignarlo al nuevo usuario
+            let code = await strapi.services["codigo-dinamico"].create({
+              Codigo: generateRandomCode(),
+            });
+            parent.codigo_dinamico = code.id;
+            parent.password = hashedPassword; // Solo asignar password al crear
+            parent.rols = ["5ff790045d6f2e272cfd7394"]; // Asignar rol solo al crear
+            parent.children = [kid.id]; // Asignar primer hijo solo al crear
 
-                    let newUser = await strapi.services.acuarelauser.create(parent);
-                    parents.push(newUser);
-                }
-            }
+            let newUser = await strapi.services.acuarelauser.create(parent);
+            parents.push(newUser);
+          }
         }
+      }
 
-        const kidEdited = await strapi.services.children.update(
-            { _id: kid.id },
-            { acuarelausers: parents.map((parent) => parent.id) }
-        );
+      const kidEdited = await strapi.services.children.update(
+        { _id: kid.id },
+        { parents_rel: parents.map((parent) => parent.id) }
+      );
 
-        return ctx.send(
-            {
-                ok: true,
-                status: 200,
-                code: 1,
-                kid: kidEdited,
-                parents,
-            },
-            200
-        );
+      return ctx.send(
+        {
+          ok: true,
+          status: 200,
+          code: 1,
+          kid: kidEdited,
+          parents,
+        },
+        200
+      );
     } else {
-        return ctx.send(validToken);
+      return ctx.send(validToken);
     }
-}
-,
+  },
   async findByPaymentTime(ctx) {
-    const { status, 'payment.time': paymentTime } = ctx.query;
-  
+    const { status, "payment.time": paymentTime } = ctx.query;
+
     const frequencyMap = {
       Diario: 1,
       Semanal: 7,
       Mensual: 30,
     };
-  
+
     const now = new Date();
-  
+
     // Helper functions
     const buildFilters = (status) => {
       const filters = {};
       if (status) filters.status = status;
       return filters;
     };
-  
+
     const sendEmail = async (correo, linkDePago) => {
       const myHeaders = {
         Cookie: "PHPSESSID=bd15560aab91a99b3aaccd6fbcecb91b",
       };
-      
+
       const formdata = new FormData();
-      
+
       formdata.append("email", correo);
       formdata.append("link", linkDePago);
-  
+
       const requestOptions = {
         method: "POST",
         headers: myHeaders,
         body: formdata,
         redirect: "follow",
       };
-  
+
       try {
         const response = await fetch(
           "https://bilingualchildcaretraining.com/s/pendingMovementsEmail/",
@@ -134,26 +136,24 @@ module.exports = {
         return false;
       }
     };
-  
+
     const processEntity = async (entity) => {
       if (entity.child) {
         const childWithMovements = await strapi.services.children.findOne({
           id: entity.child.id,
         });
         entity.child = childWithMovements;
-  
+
         const frequency = frequencyMap[entity.payment.time];
         const lastMovement = await strapi.query("movement").findOne({
           child: entity.child.id,
           _sort: "date:desc",
         });
-  
+
         if (lastMovement) {
           const lastDate = new Date(lastMovement.date);
-          const diffDays = Math.floor(
-            (now - lastDate) / (1000 * 60 * 60 * 24)
-          );
-  
+          const diffDays = Math.floor((now - lastDate) / (1000 * 60 * 60 * 24));
+
           if (diffDays >= frequency) {
             const principalParent = entity.parents.find(
               (parent) => parent.is_principal
@@ -183,19 +183,19 @@ module.exports = {
       }
       return entity;
     };
-  
+
     // Step 1: Fetch and process entities
     const filters = buildFilters(status);
     let entities = await strapi.services.inscripciones.find(filters);
     entities = await Promise.all(entities.map(processEntity));
-  
+
     // Step 2: Filter by payment time if provided
     if (paymentTime) {
       entities = entities.filter(
         (entity) => entity.payment && entity.payment.time === paymentTime
       );
     }
-  
+
     // Step 3: Categorize entities
     const categorizeEntities = (time) =>
       entities.filter(
@@ -205,65 +205,66 @@ module.exports = {
           entity.payment &&
           entity.payment.time === time
       );
-  
+
     return {
-      semanal: categorizeEntities("Semanal").map((entity) => entity.child.movements),
-      mensual: categorizeEntities("Mensual").map((entity) => entity.child.movements),
-      diario: categorizeEntities("Diario").map((entity) => entity.child.movements),
+      semanal: categorizeEntities("Semanal").map(
+        (entity) => entity.child.movements
+      ),
+      mensual: categorizeEntities("Mensual").map(
+        (entity) => entity.child.movements
+      ),
+      diario: categorizeEntities("Diario").map(
+        (entity) => entity.child.movements
+      ),
     };
   },
   async checkAndNotify(ctx) {
-      try {
-        const filters = {};
-        filters.status = "Finalizado";
-        // Obtener todas las inscripciones
-        const inscriptions = await strapi.query('inscription').find();
-  
-        const frequencyMap = {
-          Diaro: 1,
-          Semanal: 7,
-          Mensual: 30,
-        };
-  
-        for (const inscription of inscriptions) {
-          const frequency = inscription.payment.time; // 'daily', 'weekly', 'monthly'
-          const frequencyDays = frequencyMap[frequency];
-          const lastMovement = await strapi.query('movements').findOne({
-            child: inscription.child.id,
-            _sort: 'date:desc',
-          });
-  
-          const now = new Date();
-          if (lastMovement) {
-            const lastDate = new Date(lastMovement.date);
-            const diffDays = Math.floor(
-              (now - lastDate) / (1000 * 60 * 60 * 24)
-            );
-  
-            if (diffDays >= frequencyDays) {
-              // Tiempo excedido, enviar correo de notificación
+    try {
+      const filters = {};
+      filters.status = "Finalizado";
+      // Obtener todas las inscripciones
+      const inscriptions = await strapi.query("inscription").find();
 
-            }
-          } else {
-            // No tiene movimientos registrados, crear el primero en estado pendiente
-            await strapi.query('movements').create({
-              amount: inscription.payment.price,
-              date: now,
-              name: `Primer movimiento para ${inscription.child.name}`,
-              status: "2", // Estado pendiente
-              child: inscription.child.id,
-              payer: inscription.parents.find(parent => parent.is_principal).id,
-              daycare: inscription.daycare.id,
-            });
+      const frequencyMap = {
+        Diaro: 1,
+        Semanal: 7,
+        Mensual: 30,
+      };
 
+      for (const inscription of inscriptions) {
+        const frequency = inscription.payment.time; // 'daily', 'weekly', 'monthly'
+        const frequencyDays = frequencyMap[frequency];
+        const lastMovement = await strapi.query("movements").findOne({
+          child: inscription.child.id,
+          _sort: "date:desc",
+        });
+
+        const now = new Date();
+        if (lastMovement) {
+          const lastDate = new Date(lastMovement.date);
+          const diffDays = Math.floor((now - lastDate) / (1000 * 60 * 60 * 24));
+
+          if (diffDays >= frequencyDays) {
+            // Tiempo excedido, enviar correo de notificación
           }
+        } else {
+          // No tiene movimientos registrados, crear el primero en estado pendiente
+          await strapi.query("movements").create({
+            amount: inscription.payment.price,
+            date: now,
+            name: `Primer movimiento para ${inscription.child.name}`,
+            status: "2", // Estado pendiente
+            child: inscription.child.id,
+            payer: inscription.parents.find((parent) => parent.is_principal).id,
+            daycare: inscription.daycare.id,
+          });
         }
-  
-        // ctx.send({ message: 'Verificación y notificaciones completadas.' });
-        return inscriptions;
-      } catch (error) {
-        strapi.log.error('Error en checkAndNotify:', error);
       }
-    },
-  
+
+      // ctx.send({ message: 'Verificación y notificaciones completadas.' });
+      return inscriptions;
+    } catch (error) {
+      strapi.log.error("Error en checkAndNotify:", error);
+    }
+  },
 };
